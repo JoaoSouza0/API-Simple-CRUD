e64d331aed329bd298804b7cde85e7668140a8e0
Joe <joaovictorcostasouza15@gmail.com>
2020-12-07 21:15:50 -0300

    Arquivos mocks para salvar os dados

---
+++ mocks/index.js
@@ -0,0 +1,17 @@
+module.exports = [
+
+    {
+        id:1,
+        name:'João Victor'
+    },
+
+    {
+        id:2,
+        name:'Caique Alves'
+    },
+
+    {
+        id:3,
+        name:'Ricardo Cordeiro'
+    },
+]
